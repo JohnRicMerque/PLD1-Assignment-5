440fa7c65dc71b212587818566247c5a18964d35
unknown <you@example.com>
2021-11-14 07:09:27 +0800

    Successfully created a functional condition.

---
+++ Program1.py
@@ -0,0 +1,7 @@
+# the program will ask for grade percentage 
+# then displays the equivalent Grade/Mark and Description
+
+gradeP = int(input("Enter Grade Percentage: "))
+
+if gradeP >= 97 and gradeP <= 100:
+    print("Grade/Mark: 1.0\nDescription: Excellent" )
